aff2fa428879d5e955b91941260e9c471117016e
rna <rna@rna.ad.unsw.edu.au>
2015-12-21 14:59:46 +1100

    deconvolution improved

--- Deconvolution.R
+++ Deconvolution.R
@@ -1,109 +1,53 @@
-## Part I: Micro-array Data ########################################
-
+library(EDASeq)
+library(edgeR)
 library(CellMix)
 
-## micro-array data ################################################
+## Part I: Markers - Cahoy ##############################################
+cahoy_ast=read.csv("../Data/Cahoy_S4_ast.csv", header = T)
+cahoy_neu=read.csv("../Data/Cahoy_S6_neu.csv", header = T)
+
+asts <- toupper(cahoy_ast$Gene.Name)
+neus <- toupper(cahoy_neu$Gene.Name)
+common <- intersect(asts,neus)
+asts <- unique(setdiff(asts,common))
+neus <- unique(setdiff(neus,common))
+
+txtdesc <- function(x) textConnection(paste(x, collapse="\n"))
+ML_cahoy=MarkerList(file=txtdesc(c(paste(asts,"Astrocytes"),paste(neus,"Neurons"))))
+
+## Part II: Micro-array before RUV ########################################
 dataExp <- read.csv("../Data/dataExpProbes.csv")
 dataExp <- aggregate(dataExp[,-c(1,2)],by=list(dataExp$TargetID),FUN=mean)
 rownames(dataExp)<-dataExp$Group.1
 dataExp<-as.matrix(dataExp[,-1])
-
-## Torkanmani ####################################
-
-tork=read.csv("../Data/Torkamani.csv", header = T)
-asts=toupper(tork$Astrocytes[!tork$Astrocytes==""])
-neus=toupper(tork$Neurons[!tork$Neurons==""])
-common <- intersect(asts,neus)
-asts <- unique(setdiff(asts,common))
-neus <- unique(setdiff(neus,common))
-
-txtdesc <- function(x) textConnection(paste(x, collapse="\n"))
-ML_tork=MarkerList(file=txtdesc(c(paste(asts,"Astrocytes"),paste(neus,"Neurons"))))
-
-dec_tork=ged(dataExp,ML_tork,"ssKL")
-dec_tork_ssKL <- dec_tork@fit@H
-
-## Cahoy #########################################################
-cahoy_ast=read.csv("../Data/Cahoy_S4_ast.csv", header = T)
-cahoy_neu=read.csv("../Data/Cahoy_S6_neu.csv", header = T)
-
-asts <- toupper(cahoy_ast$Gene.Name)
-neus <- toupper(cahoy_neu$Gene.Name)
-common <- intersect(asts,neus)
-asts <- unique(setdiff(asts,common))
-neus <- unique(setdiff(neus,common))
-
-txtdesc <- function(x) textConnection(paste(x, collapse="\n"))
-ML_cahoy=MarkerList(file=txtdesc(c(paste(asts,"Astrocytes"),paste(neus,"Neurons"))))
-
+plotMDS(dataExp)
 dec_cahoy=ged(2^dataExp,ML_cahoy,"ssKL")
-dec_cahoy_ssKL <- dec_cahoy@fit@H
+dec_array_bRUV <- dec_cahoy@fit@H
 
-## Part II: RNA-Seq Data #######################################
+## Part III: Micro-array after RUV ########################################
+load("../Results/R1.rda")
+dataExp <- aggregate(R1[,-c(1,2)],by=list(R1$TargetID),FUN=mean)
+rownames(dataExp)<-dataExp$Group.1
+dataExp<-as.matrix(dataExp[,-1])
+plotMDS(dataExp)
+dec_cahoy=ged(2^dataExp,ML_cahoy,"ssKL")
+dec_array_aRUV <- dec_cahoy@fit@H
 
-library(DeconRNASeq)
-library(edgeR)
-
-## signatures ##################
-signatures <- read.csv("../Data/hg19.cage_peak_tpm_ann.osc_pcells_brainExpressed_0.5TPMx2_aggregatedGene.csv")
-SampleInfo <- read.csv("../Data/Fantom5SamplesAnnotated.csv")
-n<- grep("Neurons",SampleInfo$Sample)
-a<- grep("Astr",SampleInfo$Sample)
-
-nL <- SampleInfo$Library[n[1:3]]
-nS <- signatures[,colnames(signatures)%in%nL]
-aL <- SampleInfo$Library[a[4:6]]
-aS <- signatures[,colnames(signatures)%in%aL]
-
-sig <- cbind(rowMeans(nS),rowMeans(aS))
-rownames(sig) <- signatures$X
-colnames(sig) <- c("Neurons","Astrocytes")
-
-## RNA-Seq Data ##############
+## Part IV: RNA-Seq before RUV #######################################
 
 load("../Results/geneCounts.rda")
 load("../Annot/geneInfo.rda")
 
+colnames(geneCounts$counts) <- c("C2","C3","C4","R1","R2","R3")
 geneRPKM <- rpkm(x = geneCounts$counts, gene.length = geneCounts$annotation$Length)
 geneTPM <- cpm(geneCounts$counts)
 
 threshold = 0.5; nSamples = 3
 filtered <- which(rowSums(geneRPKM > threshold) >= nSamples)
+geneCounts <- geneCounts$counts[filtered,]
+plotPCA(geneCounts)
+
 TPM <- geneTPM[filtered,]
-
-TPM<-data.frame(TPM)
-TPM <- cbind(geneInfo$gene_name[match(rownames(TPM),geneInfo$gene_id)],TPM)
-colnames(TPM)[1]<-"geneName"
-TPM <- aggregate(TPM[,-1],by=list(TPM$geneName),FUN=mean)
-rownames(TPM) <- TPM$Group.1
-TPM <-TPM[,-1]
-
-common <- intersect(rownames(TPM),rownames(sig))
-TPM<-TPM[rownames(TPM)%in%common,]
-
-sig <- data.frame(sig[match(rownames(TPM),rownames(sig)),])
-
-sig <- sig[sig$Neurons>50 & sig$Astrocytes>50,]
-sig <- sig[sig$Neurons<1000&
-             sig$Astrocytes<1000,]
-TPM <- TPM[match(rownames(sig),rownames(TPM)),]
-
-## Decovlution ###################################
-output <- DeconRNASeq(datasets=TPM, signatures=sig)
-np <- output$out.all[,1]
-
-## Cahoy #################################
-
-load("../Results/geneCounts.rda")
-load("../Annot/geneInfo.rda")
-
-geneRPKM <- rpkm(x = geneCounts$counts, gene.length = geneCounts$annotation$Length)
-geneTPM <- cpm(geneCounts$counts)
-
-threshold = 0.5; nSamples = 3
-filtered <- which(rowSums(geneRPKM > threshold) >= nSamples)
-TPM <- geneTPM[filtered,]
-
 TPM<-data.frame(TPM)
 TPM <- cbind(geneInfo$gene_name[match(rownames(TPM),geneInfo$gene_id)],TPM)
 colnames(TPM)[1]<-"geneName"
@@ -112,57 +56,22 @@ rownames(TPM) <- TPM$Group.1
 TPM <-TPM[,-1]
 TPM <- as.matrix(TPM)
 
-cahoy_ast=read.csv("../Data/Cahoy_S4_ast.csv", header = T)
-cahoy_neu=read.csv("../Data/Cahoy_S6_neu.csv", header = T)
-
-asts <- toupper(cahoy_ast$Gene.Name)
-neus <- toupper(cahoy_neu$Gene.Name)
-common <- intersect(asts,neus)
-asts <- unique(setdiff(asts,common))
-neus <- unique(setdiff(neus,common))
-
-txtdesc <- function(x) textConnection(paste(x, collapse="\n"))
-ML_cahoy=MarkerList(file=txtdesc(c(paste(asts,"Astrocytes"),paste(neus,"Neurons"))))
-
 dec_cahoy=ged(TPM,ML_cahoy,"ssKL")
-dec_cahoy_ssKL <- dec_cahoy@fit@H
+dec_seq_bRUV <- dec_cahoy@fit@H
 
+## Part V: RNA-Seq after RUV #######################################
+load("../Results/FDR_seq.rda")
+geneCounts<-FDR_seq[,8:13]
+plotPCA(as.matrix(geneCounts))
+geneTPM <- cpm(geneCounts)
 
-## Cahoy #################################
-
-load("../Results/geneCounts.rda")
-load("../Annot/geneInfo.rda")
-
-geneRPKM <- rpkm(x = geneCounts$counts, gene.length = geneCounts$annotation$Length)
-geneTPM <- cpm(geneCounts$counts)
-
-threshold = 0.5; nSamples = 3
-filtered <- which(rowSums(geneRPKM > threshold) >= nSamples)
-counts <- geneCounts$counts[filtered,]
-TPM <- 2^(voom(counts)$E)
-
-TPM<-data.frame(TPM)
-TPM <- cbind(geneInfo$gene_name[match(rownames(TPM),geneInfo$gene_id)],TPM)
+TPM<-data.frame(geneTPM)
+TPM <- cbind(FDR_seq$geneNames,TPM)
 colnames(TPM)[1]<-"geneName"
 TPM <- aggregate(TPM[,-1],by=list(TPM$geneName),FUN=mean)
 rownames(TPM) <- TPM$Group.1
 TPM <-TPM[,-1]
-TPM <- as.matrix(TPM)
-
-cahoy_ast=read.csv("../Data/Cahoy_S4_ast.csv", header = T)
-cahoy_neu=read.csv("../Data/Cahoy_S6_neu.csv", header = T)
-
-asts <- toupper(cahoy_ast$Gene.Name)
-neus <- toupper(cahoy_neu$Gene.Name)
-common <- intersect(asts,neus)
-asts <- unique(setdiff(asts,common))
-neus <- unique(setdiff(neus,common))
-
-txtdesc <- function(x) textConnection(paste(x, collapse="\n"))
-ML_cahoy=MarkerList(file=txtdesc(c(paste(asts,"Astrocytes"),paste(neus,"Neurons"))))
+TPM<-as.matrix(TPM)
 
 dec_cahoy=ged(TPM,ML_cahoy,"ssKL")
-dec_cahoy_ssKL <- dec_cahoy@fit@H
-
-
-
+dec_seq_aRUV <- dec_cahoy@fit@H
